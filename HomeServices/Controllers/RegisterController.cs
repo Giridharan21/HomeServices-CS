3a082dd2f6eb6de4d7095fab6e901a93c32c17ed
fahizabot <fahijanu@gmail.com>
2020-03-11 14:32:16 +0530

    RegisterPage

---
+++ HomeServices/Controllers/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeServices.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+    
+
+    }
+}
