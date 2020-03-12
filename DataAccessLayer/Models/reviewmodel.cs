50494d89e0ed6bbee705ba161c3844480aab0a93
Navya394 <navyaramisetty11@gmail.com>
2020-03-12 17:59:45 +0530

    Review-done

---
+++ DataAccessLayer/Models/reviewmodel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class reviewmodel
+    {
+        public int OrderIdFK { get; set; }
+        [Required(ErrorMessage ="Custom Required Field")]
+        [Range(1.0,5.0,ErrorMessage ="Enter the number between 1 to 5\n")]
+        public decimal Stars { get; set; }
+        [Required]
+        [StringLength(256, ErrorMessage = "comments cannot be longer than 256 charaters\n")]
+        public string Comment { get; set; }
+
+
+    }
+}
---
+++ DataAccessLayer/m/stars.cs
@@ -0,0 +1,6 @@
+﻿namespace m
+{
+    internal class stars
+    {
+    }
+}
