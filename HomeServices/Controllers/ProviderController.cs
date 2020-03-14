3fba8ac7b5d45ac7830544f0ce457c093608fdbf
mehataj279 <mehatajpattan860@gmail.com>
2020-03-14 14:42:17 +0530

    deleting old serviceprovider and created new one

---
+++ HomeServices/Controllers/ProviderController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer.Models;
+using DataAccessLayer;
+
+namespace HomeServices.Controllers
+{
+    public class ProviderController : Controller
+    {
+        // GET: Provider
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult Receivedorder()
+        {
+
+            var res = Data.ServiceProvider();
+            return View(res);
+        }
+
+        [HttpPost]
+        public ActionResult Receivedorder(int OrderId, string Status)
+        {
+            Data.Accept(OrderId, Status);
+            return View();
+        }
+
+        public ActionResult Previousorder()
+        {
+            var res = Data.ServiceProvider();
+            return View(res);
+
+        }
+    }
+}
